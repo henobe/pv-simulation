bffa4a3b835b5e5ea90611c01d0b14236da30135
henobe <47500819+henobe@users.noreply.github.com>
2019-02-27 13:45:12 +0100

    Flächenberechnung über Zeitachse eingeführt

--- quickEstimate.R
+++ quickEstimate.R
@@ -17,16 +17,17 @@ calculateEfficiency <- function(alpha, beta){
 
 # https://www.sunearthtools.com/dp/tools/pos_sun.php
 
-solarWinkel <- read_delim('SunPath.csv', delim = ";", skip = 3) %>%
+solar_winkel <- read_delim('SunPath.csv', delim = ";", skip = 3) %>%
   filter(Elevation > 0) %>%
+  mutate(time_diff_sec = Stunde - Stunde[[1]]) %>%
   #mutate_at(c("Azimuth"), diff180) %>%
   mutate_at(c("Azimuth"), function(x) x-180) %>% # Lösung über anonyme Funktion, um auszuprobieren
   mutate(ElevationRad = gradToRad(Elevation)) %>%
   mutate(AzimuthRad = gradToRad(Azimuth)) %>%
   mutate(projectedArea = calculateEfficiency(ElevationRad, AzimuthRad))
 
-ggplot(solarWinkel, aes(x = Azimuth, y = Elevation)) +
-  geom_label(data = solarWinkel[seq(1, nrow(solarWinkel), 10),], aes(label = Stunde)) +
+ggplot(solar_winkel, aes(x = Azimuth, y = Elevation)) +
+  geom_label(data = solar_winkel[seq(1, nrow(solar_winkel), 10),], aes(label = Stunde)) +
   geom_line(alpha = 0.5) +
   geom_point(size = 0.5) +
   scale_y_continuous(limits = c(0,90)) +
@@ -34,7 +35,7 @@ ggplot(solarWinkel, aes(x = Azimuth, y = Elevation)) +
 
 #ggsave("sonnenstand.png", width = 10, height = 5, dpi = 200)
 
-ggplot(solarWinkel, aes(x = Stunde, y = projectedArea)) +
+ggplot(solar_winkel, aes(x = Stunde, y = projectedArea)) +
   geom_line() +
   geom_hline(yintercept = 1) +
   labs(
@@ -45,6 +46,8 @@ ggplot(solarWinkel, aes(x = Stunde, y = projectedArea)) +
 
 #ggsave("einstrahlfläche.png", width = 10, height = 5, dpi = 200)
 
+nachfuehr_wert <- berechne_kurvenflaeche(solar_winkel$time_diff_sec, solar_winkel$projectedArea)
+liegen_wert <- berechne_kurvenflaeche(solar_winkel$time_diff_sec, 1)
 # "Tagesbilanz", also Prozentualer Gesamtunterschied berechnen
 # Optimierungsfunktion für besten Starren Winkel
 # Strahlung mit "1 Watt"
