d67ea65e4efff589141318f1a50eaa0b4ab629f5
henobe <47500819+henobe@users.noreply.github.com>
2019-02-27 19:16:03 +0100

    Winkeleffizienzberechnung verständlicher und neue Herangehensweise
    Die eingegebenen Daten für den Einstrahlhöhenwinkel und -azimuthwinkel müssen nun nicht mehr "vorbereitet" werden sondern werden in der Funktion angepasst.

--- berechne_winkeleffizienz.R
+++ berechne_winkeleffizienz.R
@@ -1,9 +1,14 @@
-berechne_winkeleffizienz <- function(elevation_winkel, elevation, azimuth){
+berechne_winkeleffizienz <- function(kipp_winkel, einstrahl_hoehenwinkel, einstrahl_azimuthwinkel){
   
-  new_el_winkel <- elevation - elevation_winkel
+  relativer_optimaler_kippwinkel <- pi/2 - einstrahl_hoehenwinkel - kipp_winkel
+  # 90 Grad + Einstrahlwinkel + "optimaler Ausrichtwinkel" ergeben stets 180 Grad
+  # der relative optimale Kippwinkel berücksichtigt zusätzlich die Kippung
   
-  x <- cos(new_el_winkel)
-  y <- cos(azimuth)
+  relativer_optimaler_azimuthwinkel <- einstrahl_azimuthwinkel - pi
+  # der Azimuthwinkel wird ausgehend von einem Kreismit 0 Richtung Norden angegeben. 
+  # um den Richtwert "0" im Süden zu haben, wird der Winkel um 180° gedreht
+  # Eine Einstellung des Azimuthwinkels ist nicht vorgesehen,
+  # es wird von einer Standardausrichtugn nach Süden ausgegangen
   
-  x*y
+  cos(relativer_optimaler_kippwinkel) * cos(relativer_optimaler_azimuthwinkel)
 }
