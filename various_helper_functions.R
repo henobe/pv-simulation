705aa49a5296ab73ff512ae96ed088db20275618
henobe <47500819+henobe@users.noreply.github.com>
2019-03-18 11:01:45 +0100

    rename winkelumrechnungsfunktionen

--- various_helper_functions.R
+++ various_helper_functions.R
@@ -1,9 +1,9 @@
 # Collection of small reusable functions
 
-grad_zu_rad <- function(value){
+wechsel_grad_zu_rad <- function(value){
   value*pi/180
 }
 
-rad_zu_grad <- function(x){
+wechsel_rad_zu_grad <- function(x){
   x*180/pi
 }
