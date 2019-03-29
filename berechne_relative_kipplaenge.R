2744c3e9ce2d0f65f2c9fb69b3537fadc07f9b4c
henobe <47500819+henobe@users.noreply.github.com>
2019-03-29 19:02:41 +0100

    Fix error in optimal kippwinkel
    Die automatische Auswahl des Kippwinkels war fehlerhaft. Hier wurde der Einstrahlwinkel einfach übernommen, anstatt 90° - Eienstrahlwinkel zu rechnen. Eine benutzung der Funktion mit Einstrahlwinkeln > 90° ist weiterhin undefiniert.

--- berechne_relative_kipplaenge.R
+++ berechne_relative_kipplaenge.R
@@ -1,4 +1,4 @@
-berechne_relative_kipplaenge <- function(einstrahlwinkel, kippwinkel = einstrahlwinkel) {
+berechne_relative_kipplaenge <- function(einstrahlwinkel, kippwinkel = (pi/2 - einstrahlwinkel)) {
   # ohne Input wird direkt die relative Länge bei optimaler Ausrichtung berechnet.
   
   if (einstrahlwinkel > (pi/2) || kippwinkel > (pi/2)) 
