eddb425516406137f54f876844d9c84afa0ed69d
henobe <47500819+henobe@users.noreply.github.com>
2019-04-29 12:14:58 +0200

    Remove abs() function
    Die Berechnung des Betrages an der Stelle in der Funktion schien weder mathematisch sinnvoll noch in seiner Funktion an dieser Stelle angebracht. Besser als Transformation der Daten als direkt innerhalb der Berechnung.

--- berechne_relative_kipplaenge.R
+++ berechne_relative_kipplaenge.R
@@ -6,10 +6,10 @@ berechne_relative_kipplaenge <- function(einstrahlwinkel, kippwinkel = (pi/2 - e
   einstrahlwinkel <- ifelse((einstrahlwinkel %% pi) == 0, NaN, einstrahlwinkel)
   # Division mit 0 vermeiden
   
-  abs(sin(einstrahlwinkel + kippwinkel) / sin(einstrahlwinkel))
+  relative_length <- (sin(einstrahlwinkel + kippwinkel) / sin(einstrahlwinkel))
   # see documentation for reason behind calculation
-  # Der Betrag, weil Länge immer positiv ist (tritt auf bei Winkel > 180 oder <0)
 
   # Issues: 
   # - bei Mehrfachen von pi wird die Berechnung unzulässig ungenau
+  # - Wie wird mit negativen Werten umgegangen? Muss noch geklärt werden.
 }
