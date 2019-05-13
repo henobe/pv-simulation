11ed40f4e510ca67b379304b5b59eea9fd7618e0
henobe <47500819+henobe@users.noreply.github.com>
2019-05-13 14:19:46 +0200

    Add angle functions for skalar and kartesian conversion

--- various_helper_functions.R
+++ various_helper_functions.R
@@ -7,3 +7,37 @@ wechsel_grad_zu_rad <- function(value){
 wechsel_rad_zu_grad <- function(x){
   x*180/pi
 }
+
+wechsel_polar_zu_kartesisch <- function(azimuth, elevation, length=1){
+  # NED-System:
+  # x zeigt nach Norden, also azimuth = 0, elevation = 90
+  # y zeigt nach Osten, also azimuth = 90, elevation = 90
+  # z zeigt nach "Unten", also azimuth egal und elevation = 0
+  
+  # check for correct input
+  #error <- ifelse(0>azimuth | azimuth>(2*pi) | (-pi/2) > azimuth | azimuth > (pi/2), TRUE, FALSE)
+  #if(error){
+  #  stop("Input angle out of range")
+  #}
+  # --> doesn't work vectorised
+  
+  
+  x <- length * sin(elevation) * cos(azimuth)
+  y <- length * sin(elevation) * sin(azimuth)
+  z <- length * cos(elevation)
+  
+  # Um die Geschwindigkeit und Zuverlässigkeit zu erhöhen, wird der Rundungsfehler um 0
+  # minimiert, in dem ab einem Wert sehr nahe an mit 0 weitergerechnet wird.
+  #x <- ifelse(x < 1e-15, 0, x)
+  #y <- ifelse(y < 1e-15, 0, y)
+  #z <- ifelse(z < 1e-15, 0, z)
+  
+  list(x, y, z)
+}
+
+berechne_skalarprodukt <- function(vector_a, vector_b){
+  vector_a <- unlist(vector_a)
+  vector_b <- unlist(vector_b)
+  
+  drop(vector_a %*% vector_b)
+}
