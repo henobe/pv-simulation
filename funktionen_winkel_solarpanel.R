fa115c4a72b3f33062a425c344c1ddc4551e115e
henobe <47500819+henobe@users.noreply.github.com>
2019-07-23 14:41:57 +0200

    Develop function to calculate normal vector
    Correct calculation of azimuth.
    Expand function to handle vectorised inputs.
    Create test file.

--- funktionen_winkel_solarpanel.R
+++ funktionen_winkel_solarpanel.R
@@ -1,6 +1,6 @@
 # Verschiedene Funktionen zu Winkelrechnungen des Solarpanels
 
-berechne_normalenvektor_panel <- function(drehwinkel=pi, 
+berechne_normalenvektor_panel <- function(drehwinkel=0, 
                                           kippwinkel=0, 
                                           radiant = TRUE){
   # INPUT: Kippwinkel und Drehwinkel eines Panels
@@ -12,7 +12,7 @@ berechne_normalenvektor_panel <- function(drehwinkel=pi,
   # drehwinkel entspricht azimuth, 0° -> Norden
   #   Dabei betrachtet man den Winkel von der gekippten Kante
   #   zur am Boden liegenden Kante. Ein Panel, gekippt und nach
-  #   Süden ausgerichtet, hat also einen Drehwinkel von 180°.
+  #   Süden ausgerichtet, hat also einen Drehwinkel von 0°!.
   # kippwinkel entspricht Kippung des Panels zum Boden:
   #   also 0° -> flach auf Boden, 90° -> senkrecht auf Boden
   
@@ -22,29 +22,22 @@ berechne_normalenvektor_panel <- function(drehwinkel=pi,
   }
   
   # Dadurch dass der Drehwinkel bereits nach Definition "vom
-  # Panel wegezeigt", muss dieser nicht mehr verändert werden,
-  # sondern ist identisch dem Azimuth des Normalenvektor
-  normalenvektor_azimuth = drehwinkel
+  # Panel wegzeigt", also um 180° gedreht ist,
+  # muss der Normalenvektor im NED-System nur um 180° gedreht werden.
+  normalenvektor_azimuth = pi + drehwinkel
   
   # Der Höhenwinkel ist aufgrund der verschiedenen Definitionen bzw.
   # Bezugssysteme noch von 180° abzuziehen. Der Normalenvektor
   # steht daraufhin mit 90° vom Panel ab.
   normalenvektor_elevation = pi - kippwinkel
 
-  (normalenvektor <- polar_zu_kartesisch(azimuth = normalenvektor_azimuth,
-                                         elevation = normalenvektor_elevation))
-}
-
-
-berechne_panelwinkel_in_skalar <- function(kippwinkel = 0){
-  
-  transpose((polar_zu_kartesisch(pi, grad_zu_rad(180-kippwinkel))))
-  
-  #transpose((wechsel_polar_zu_kartesisch(wechsel_grad_zu_rad(180 + diffsuedwinkel), wechsel_grad_zu_rad(kippwinkel-90))))
-  
-  #data <- ((wechsel_polar_zu_kartesisch(wechsel_grad_zu_rad(180 + diffsuedwinkel), wechsel_grad_zu_rad(kippwinkel))))
-  
-  #data <- lapply(data, function(x) -x)
-  
-  #transpose(data)
+  if(length(drehwinkel) > 1){
+    return(vectorised_polar_zu_kartesisch(azimuth = normalenvektor_azimuth,
+                               elevation = normalenvektor_elevation,
+                               length = 1))
+  } else {
+    return(polar_zu_kartesisch(azimuth = normalenvektor_azimuth,
+                               elevation = normalenvektor_elevation,
+                               length = 1))
+  }
 }
--- tests/test_funktion_normalenvektor.R
+++ tests/test_funktion_normalenvektor.R
@@ -1,21 +1,67 @@
 # Testen der Funktion zur Berechnung des Normalenvektors des Solarpanels
 library(testthat)
 source('funktionen_winkel_solarpanel.R')
+source('funktionen_helper_winkel.R') # für Funktionen innerhalb der Funktion
+
+
+test_that("Datenformat ist korrekt", {
+  expect_true(is.vector(berechne_normalenvektor_panel()))
+  expect_true(is.numeric(berechne_normalenvektor_panel()))
+  expect_equal(length(berechne_normalenvektor_panel()), 3)
+  expect_equal(names(berechne_normalenvektor_panel()), c("north", "east", "down"))
+})
 
 
 test_that("Standardwert ist flach liegend", {
-  expect_equal(berechne_normalenvektor_panel(), list(0,0,-1))
+  expect_equal(unname(berechne_normalenvektor_panel()), c(0,0,-1))
+            # 'unname' damit die Vektoren verglichen werden koennen
 })
+
 
 test_that("Drehung bei flach liegend ist irrelevant", {
-  expect_equal(berechne_normalenvektor_panel(1, 0), list(0,0,-1))
-  expect_equal(berechne_normalenvektor_panel(2, 0), list(0,0,-1))
-  expect_equal(berechne_normalenvektor_panel(3, 0), list(0,0,-1))
+  expect_equal(unname(berechne_normalenvektor_panel(1, 0)), c(0,0,-1))
+  expect_equal(unname(berechne_normalenvektor_panel(2, 0)), c(0,0,-1))
+  expect_equal(unname(berechne_normalenvektor_panel(3, 0)), c(0,0,-1))
 })
 
-test_that("Normalenvektor ist Drehwinkel bei 90-Kippung", {
-  expect_equal(berechne_normalenvektor_panel(1, pi), polar_zu_kartesisch(1,0))
-  expect_equal(berechne_normalenvektor_panel(2, pi), polar_zu_kartesisch(2,0))
-  expect_equal(berechne_normalenvektor_panel(3, pi), polar_zu_kartesisch(3,0))
+
+test_that("Normalenvektor ist Drehwinkel + 180 Grad bei 90-Kippung", {
+  expect_equal(unname(berechne_normalenvektor_panel(0, pi/2)), c(-1,0,0))
+  expect_equal(unname(berechne_normalenvektor_panel(pi/2, pi/2)), c(0,-1,0))
+  expect_equal(unname(berechne_normalenvektor_panel(pi, pi/2)), c(1,0,0))
+  expect_equal(unname(berechne_normalenvektor_panel(3/2*pi, pi/2)), c(0,1,0))
 })
 
+
+test_that("Normalenvektor ist mathematisch korrekt", {
+  expect_equal(berechne_normalenvektor_panel(1,  0.1), polar_zu_kartesisch(pi + 1, pi - 0.1))
+  expect_equal(berechne_normalenvektor_panel(1, -0.1), polar_zu_kartesisch(pi + 1, pi + 0.1))
+  expect_equal(berechne_normalenvektor_panel(-0.5,  0.2), polar_zu_kartesisch(pi - 0.5, pi - 0.2))
+  expect_equal(berechne_normalenvektor_panel(-0.5, -0.2), polar_zu_kartesisch(pi - 0.5, pi + 0.2))
+})
+
+
+test_that("Umrechnung bei Benutzung von 'Grad' funktioniert", {
+  expect_equal(berechne_normalenvektor_panel(rad_zu_grad(1), rad_zu_grad(0.1), radiant = FALSE),
+               polar_zu_kartesisch(pi + 1, pi - 0.1))
+  expect_equal(berechne_normalenvektor_panel(rad_zu_grad(1), rad_zu_grad(-0.1), radiant = FALSE), 
+               polar_zu_kartesisch(pi + 1, pi + 0.1))
+  expect_equal(berechne_normalenvektor_panel(rad_zu_grad(-0.5), rad_zu_grad(0.2), radiant = FALSE), 
+               polar_zu_kartesisch(pi - 0.5, pi - 0.2))
+  expect_equal(berechne_normalenvektor_panel(rad_zu_grad(-0.5), rad_zu_grad(-0.2), radiant = FALSE), 
+               polar_zu_kartesisch(pi - 0.5, pi + 0.2))
+})
+
+
+test_that("Output ist korrekt bei vektorisiertem Input", {
+  drehwinkel <- c(1,1,-0.5, -0.5)
+  kippwinkel <- c(0.1,-0.1,0.2, -0.2)
+  
+  erwartete_koords <- list(polar_zu_kartesisch(pi + 1, pi - 0.1),
+                           polar_zu_kartesisch(pi + 1, pi - - 0.1),
+                           polar_zu_kartesisch(pi - 0.5, pi - 0.2),
+                           polar_zu_kartesisch(pi - 0.5, pi - -0.2))
+  
+  expect_equal(berechne_normalenvektor_panel(drehwinkel, kippwinkel),
+               erwartete_koords)
+})
