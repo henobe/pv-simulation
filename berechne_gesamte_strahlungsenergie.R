b6db7ca55169945ceee279fd16043a0379daa1fa
Hendrik Obeloeer <hendrik.obeloeer@ane.energy>
2019-11-08 16:58:52 +0100

    Unify format

--- berechne_gesamte_strahlungsenergie.R
+++ berechne_gesamte_strahlungsenergie.R
@@ -1,12 +1,12 @@
 # Berechne Gesamte Strahlnugsenergie
 
-berechne_gesamte_strahlungsenergie <- function (strahlungsenergie_pro_flaeche, 
-                                                relative_flaeche) {
+berechne_gesamte_strahlungsenergie <- function(strahlungsenergie_pro_flaeche, 
+                                               relative_flaeche) {
   # INPUT: relative Einstrahlflaeche, Einstrahlenergie pro Flaeche,
   #        beide Werte müssen gleich lang sein
   # OUTPUT: Summe der gesamten Strahlungsenergie, 
-
-  strahlungswerte <- (strahlungsenergie_pro_flaeche * relative_flaeche)
+  
+  strahlungswerte <- strahlungsenergie_pro_flaeche * relative_flaeche
   
   sum(strahlungswerte[strahlungswerte > 0]) # neg. Werte nicht beachten
 }
