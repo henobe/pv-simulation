78f661c133fab5492218b36d108e5dcd783f8cf6
Hendrik Obeloeer <hendrik.obeloeer@ane.energy>
2019-12-18 22:48:03 +0100

    Add report to investigate speed

--- reports/funktionen_winkel_optimierung_fast.R
+++ reports/funktionen_winkel_optimierung_fast.R
@@ -1,12 +1,9 @@
-# Funktionen zur Berechnung der optimalen Dreh- und Kippwinkel
 
-berechne_optimale_panelwinkel <- function(sonnenwinkel,
-                                          strahlungsenergie_pro_flaeche) {
+berechne_optimale_panelwinkel_fast <- function(sonnenwinkel,
+                                               strahlungsenergie_pro_flaeche) {
   # INPUT: - Sonnenwinkel: Liste der Sonnenwinkel in kart. Koord
   #        - Strahlungsenergie: Vektor mit passenden Strahlungswerten
-  #        - Startwinkel: Start für den Optimierungsalgorithmus
-  # OUTPUT: Optimaler Azimuth und Elevation Winkel
-  #         Vektor, mit diesen beiden Elementen
+  # OUTPUT: Optimaler Elevation Winkel
   
   gesamte_strahlungsenergie <- function(elevation,
                                         azimuth,
@@ -17,47 +14,29 @@ berechne_optimale_panelwinkel <- function(sonnenwinkel,
                                        irr,
                                        berechne_strahlungsenergie_bei_panelwinkel,
                                        elevation = elevation,
-                                       azimuth = azimuth)
+                                       azimuth = 0)
     
     sum(eingefangene_strahlung)
   }
   
   result <- optimise(gesamte_strahlungsenergie,
                      interval = c(-90, 90),
-                     tol = 0.01,
+                     tol = 0.05,
                      maximum = TRUE,
-                     azimuth = 0,
                      sw = sonnenwinkel,
                      irr = strahlungsenergie_pro_flaeche)
   
-  rlang::set_names(c(result[[1]], 0), c("elevation", "azimuth"))
+  c("elevation" = result[[1]], 
+    "azimuth" = 0)
 }
 
 
-get_optimised_intervall_length <- function(start_date, end_date) {
-  days_label <- c("below 20", "20-30", "30-60", "60-90",
-                  "90-120", "120-150", "above 150")
-  days_number <- c(0, 20, 30, 60, 90, 120, 150, 9999)
-  
-  lookuptable <- tibble(number_days = days_label,
-                        # minutes:
-                        intervall_length = c(10, 20, 30, 40, 50, 60, 90)) %>%
-    mutate_at("number_days", factor)
-  
-  deltap <- cut(as.numeric(end_date - start_date), 
-                breaks = days_number, labels = days_label)
-  
-  index <- match(deltap, lookuptable$number_days)
-  
-  lookuptable$intervall_length[index]
-}
 
-
-berechne_optimale_panelwinkel_gesamt <- function(start_date = now(),
-                                                 end_date = now() + days(1),
-                                                 lat = 53.6332,
-                                                 lon = 9.9881,
-                                                 intervall_length = 10) {
+berechne_optimale_panelwinkel_gesamt_fast <- function(start_date = now(),
+                                                      end_date = now() + days(1),
+                                                      lat = 53.6332,
+                                                      lon = 9.9881,
+                                                      intervall_length = 10) {
   # INPUT: start_date und end_date als POSIXct-Wert,
   #        postion als vector mit zwei Dezimalkoordinaten,
   #        intervall_length: Abstand zwischen zwei Messpunkten, in Minuten 
@@ -92,10 +71,10 @@ berechne_optimale_panelwinkel_gesamt <- function(start_date = now(),
              lon = lon,
              zenith_angle = zenith_angle,
              seasonal_accuracy = FALSE)
-           )
+    )
   t3 <- Sys.time()
-  optimale_winkel <- berechne_optimale_panelwinkel(df$winkel_kartesisch,
-                                                   df$sonnen_strahlung)  
+  optimale_winkel <- berechne_optimale_panelwinkel_fast(df$winkel_kartesisch,
+                                                        df$sonnen_strahlung)  
   t4 <- Sys.time()
   df <- mutate(df,
                eingefangene_strahlung = map2_dbl(
@@ -104,7 +83,7 @@ berechne_optimale_panelwinkel_gesamt <- function(start_date = now(),
                  berechne_strahlungsenergie_bei_panelwinkel,
                  elevation = optimale_winkel["elevation"],
                  azimuth = optimale_winkel["azimuth"])
-               )
+  )
   t5 <- Sys.time()
   gain <- (sum(df$eingefangene_strahlung) / sum(df$sonnen_strahlung) - 1) * 100
   
