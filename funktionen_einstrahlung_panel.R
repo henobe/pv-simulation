b7dfe1106b57b6adb5449adc41151fe8b8d58945
Hendrik Obeloeer <hendrik.obeloeer@ane.energy>
2019-12-18 23:19:40 +0100

    Add ability to calculate irridation with tracking

--- funktionen_einstrahlung_panel.R
+++ funktionen_einstrahlung_panel.R
@@ -3,19 +3,24 @@
 berechne_strahlungsenergie_bei_panelwinkel <- function(elevation,
                                                        azimuth = 0,
                                                        sw,
-                                                       irr) {
+                                                       irr,
+                                                       tracking = FALSE) {
   # INPUT: - x, Angles: Vektor mit Azimuth und Elevationwinkel in Grad
   #        - sw, Sonnenwinkel: Liste der Sonnenwinkel in kart. Koord
   #        - irr, Irridation: Vektor mit passenden Strahlungswerten
+  #        - tracking: does the panel adjust its angels optimally to the sun?
   # OUTPUT: Gesamte Strahlungsenergie (Skalar),
   #         die bei dieser Konfiguration aufgefangen worden wäre
   
-  normalenvektor_panel <- berechne_normalenvektor_panel(kippwinkel = elevation,
-                                                        drehwinkel = azimuth,
-                                                        radiant = FALSE)
-  
-  relative_flaeche <- berechne_relative_einstrahlflaeche(sw,
-                                                         normalenvektor_panel)
-  
+  if (!tracking){
+    normalenvektor_panel <- berechne_normalenvektor_panel(kippwinkel = elevation,
+                                                          drehwinkel = azimuth,
+                                                          radiant = FALSE)
+    
+    relative_flaeche <- berechne_relative_einstrahlflaeche(sw,
+                                                           normalenvektor_panel)
+  } else {
+    relative_flaeche <- berechne_relative_einstrahlflaeche(sw)
+  }
   if_else(relative_flaeche < 0, 0, irr * relative_flaeche)
 }
